d2435873f05fea191befbed719856ca063f68db3
yymin1022 <yymin1022@gmail.com>
2019-04-12 14:53:41 +0900

    Initialize Project

---
+++ app/src/main/java/com/cecom/alchol/CommunityActivity.java
@@ -0,0 +1,13 @@
+package com.cecom.alchol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CommunityActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community);
+    }
+}
---
+++ app/src/main/java/com/cecom/alchol/RecipeResultActivity.java
@@ -0,0 +1,14 @@
+package com.cecom.alchol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipeResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_result);
+    }
+}
