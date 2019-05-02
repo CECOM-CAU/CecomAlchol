8801e39b0d0c9d8765744310e70da29a79c60b77
splo2t <yu990410@naver.com>
2019-05-02 17:53:14 +0900

    add branch splo2t

--- app/src/main/java/com/cecom/alchol/CommunityActivity.java
+++ app/src/main/java/com/cecom/alchol/CommunityActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CommunityActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community);
     }
 }
+
