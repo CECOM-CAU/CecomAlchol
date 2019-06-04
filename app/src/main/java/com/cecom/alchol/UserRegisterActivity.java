bfa072664277ee7dde91bb6ab1e6009da076a020
splo2t <yu990410@naver.com>
2019-06-04 19:16:40 +0900

    edit error

--- app/src/main/java/com/cecom/alchol/UserRegisterActivity.java
+++ app/src/main/java/com/cecom/alchol/UserRegisterActivity.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
